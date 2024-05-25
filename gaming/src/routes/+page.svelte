94a87393150d347d2d2dcb257fd6271b25133a43
Vik Sir <vsirotkin15@gmail.com>
2024-05-25 15:25:31 +0300

    on:click and bind functionality

--- gaming/src/routes/+page.svelte
+++ gaming/src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let title = 'New Title for Gaming';
+
+    const changeTitle = () => {
+        title = 'Title Changed';
+    }
+</script>
+
+<main>
+	<div class="index">
+		<h2>{title}</h2>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe eum nesciunt provident minus
+			dicta ad itaque aperiam odit fuga architecto quos enim tenetur voluptatem beatae quod cumque
+			facere expedita, id repellendus voluptatibus. Officiis eligendi voluptate a porro! Minus
+			veniam suscipit rerum laboriosam, distinctio tenetur. Ex quia expedita temporibus nihil eos?
+		</p>
+
+        <button on:click={changeTitle}>Change Title</button>
+        <input type="text" bind:value={title}>
+	</div>
+</main>
+
+<style>
+    .index {
+        display: block;
+        text-align: center;
+        margin: 20px auto;
+    }
+</style>
